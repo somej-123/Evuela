3df8c51ccf2007703b210dfddabd01866625f7ca
somej-123 <somej@naver.com>
2022-10-17 23:46:50 +0900

    Blog 모바일 메뉴 제작 진행중

--- blog/home.php
+++ blog/home.php
@@ -24,8 +24,8 @@
     <!-- blog header css,js 경로 끝 -->
 
     <!-- blog css,js 경로 -->
-    <!-- <link href="./css/home.css" rel="stylesheet"/>
-    <script src="./js/home.js"></script> -->
+    <link href="./css/main.css" rel="stylesheet"/>
+    <!-- <script src="./js/home.js"></script> -->
     <!-- blog css,js 경로 끝-->
 
     <title>EVUELA::BLOG</title>
@@ -45,13 +45,14 @@
             <!-- Blog Header 메뉴 시작 -->
             <div id="headerRightContents">
                 <div class="row" style="margin: 0; padding:0; height:100%">
-                    <div class="col-8" style="margin: 0; padding:0;">
+                    <div class="col-9" style="margin: 0; padding:0;">
                        <div id="headerMenuFlexBox">
                             <div id="headerMenuInfoDiv">
                                 <p id="headerMenuInfoP" onclick="location.href='../info/home'">INFO</p>
                             </div>
                             <div id="headerMenuBlogDiv">
-                                <p id="headerMenuBlogP" onclick="location.href='../blog/home'">BLOG</p>
+                                <p id="headerMenuBlogP" class="selectMenu" onclick="location.href='../blog/home'">BLOG</p>
+                                <div id="selectMenuLine"></div>
                             </div>
                             <div id="headerMenuPictureDiv">
                                 <p id="headerMenuPictureP">PICTURE</p>
@@ -61,22 +62,24 @@
                             </div>
                        </div>
                     </div>
-                    <div class="col-4" style="margin: 0; padding:0;">
+                    <div class="col-3" style="margin: 0; padding:0;">
                         <div id="headerUserMenuBox">
                             <?php if(!isset($_SESSION["signin"])){?>
-                                <p>LOGIN</p>
+                                <p onclick="location.href='../login/home'">LOGIN</p>
                             <?php }
                             else{ ?>
                             <p id="headerUserMenuP">MENU <i class="fas fa-caret-down"></i></p>
                             <div id="headerUserMenuDiv" style="display: none;">
-                                <ul>
-                                    <li>
-                                        <a href="#">프로필</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">로그아웃</a>
-                                    </li>
-                                </ul>
+                                <div class="headerUserMenuBorder" id="headerMenuProfileDiv">
+                                    <p id="headerMenuProfileP" onclick="location.href='../setting/home'">
+                                        PROFILE
+                                    </p>
+                                </div>
+                                <div class="headerUserMenuBorder" id="headerMenuLogoutDiv">
+                                    <p id="headerMenuLogoutP" onclick="location.href='../login/userSignout'">
+                                        LOGOUT
+                                    </p>
+                                </div>
                             </div>
                             <?php } ?>
                         </div>
@@ -84,12 +87,32 @@
                 </div>
             </div>
             <!-- Blog Header 메뉴 끝-->
+
+            <!-- Blog Mobile Header 시작 -->
+
+            <div id="headerRightContents_M">
+                <div id="headerRightMenubar_M">
+                    <i id="menubar_M" class="fas fa-2x fa-bars"></i>
+                </div>
+                <div id="sideMenu_M">
+                    <div id="sideMenuHeader_M" class="row">
+                        <div class="col-6 sideMenuHeaderContents_M">
+                            <p>PROFILE</p>
+                        </div>
+                        <div class="col-6 sideMenuHeaderContents_M">
+                            <p>LOGOUT</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Blog Mobile Header 끝 -->
         </nav>
     </header>
     <!-- Blog Header 끝 -->
 
     <!-- Blog body 섹션 -->
-    <section>
+    <section id="testMenu">
 
     </section>
     <!-- Blog Body 섹션 끝 -->
--- info/js/header.js
+++ info/js/header.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
     $("#mobile_navMenuToggleBtn").on("click",function(){
         $("#mobile_navMenu").fadeToggle(1000);
     });
+
+    $( window ).resize(function() {
+        //창크기 변화 감지
+        open_chatroom();
+     });
 })
+
+
+
+function open_chatroom(){
+    var windowWidth = $( window ).width();
+    if(windowWidth > 768) {
+       $("#mobile_navMenu").css("display","none");
+    }
+ }
